e25dfd60a2fb39e27b0ac36bc2e40d103c6fef69
bz <bzx@tuta.io>
2021-01-16 10:03:47 -0300

    removendo comments

--- main.py
+++ main.py
@@ -32,13 +32,7 @@ def command_handler(update: Update, context: CallbackContext) -> None:
 
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(MessageHandler(Filters.text, command_handler))
@@ -46,9 +40,6 @@ def main():
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
