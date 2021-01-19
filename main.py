480cb9408409d743e41a5eea51c9460dd83270db
bz <bzx@tuta.io>
2021-01-19 20:25:56 -0300

    paste code integration added, error handler with feedback implemented

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import logging
 from secrets import token_hex
+from requests import post
+from requests.packages import urllib3
+from json import dumps
+from random import randint
+import html
+import json
+import logging
+import traceback
 
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -16,12 +24,21 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+# ignora aviso de conexao insegura(para snippets.glot.io)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+codepaste_headers = {'Authorization': 'Token c98f9e90-07df-413c-b7a1-8d6e7b6bb1d2',
+                     'Content-type': 'application/json'}
+
 
 def echoer(update: Update, context: CallbackContext) -> None:
     print(update)
 
 
 def command_handler(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    message_id = update.message.message_id
+
     message = update.message.text
     command = ''.join(message.split('@')[:1]).split(' ')[0]
     message = escape_markdown(' '.join(message.split(' ')[1:]))
@@ -91,6 +108,63 @@ def command_handler(update: Update, context: CallbackContext) -> None:
             text=response
         )
 
+    elif command in ['/code', '/paste']:
+        lang = message.split(' ')[0]
+        if lang not in ['python', 'javascript', 'c', 'rust', 'py']:
+            lang = 'plaintext'
+        snippet = ' '.join(message.split(' ')[1:])
+
+        data = {
+            "language": lang,
+            "title": f'file-{randint(1, 100)}',
+            "public": 'false',
+            "files": [
+                {
+                    "name": f'file-{randint(1, 100)}.{lang}',
+                    "content": snippet
+                }
+            ]
+        }
+
+        req = post(
+            url='https://snippets.glot.io/snippets',
+            headers=codepaste_headers,
+            data=dumps(data),
+            verify=False
+        )
+
+        if req.status_code == 200:
+            snippet_id = req.json()['id']
+            response = f'Código salvo em: https://glot.io/snippets/{snippet_id}'
+        else:
+            response = 'Erro salvando snippet!'
+
+        update.message.reply_html(
+            text=response
+        )
+
+        # deleta a mensagem com o código
+        context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+
+
+def error_handler(update: Update, context: CallbackContext) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = ''.join(tb_list)
+
+    message = (
+        f'An exception was raised while handling an update\n'
+        f'<pre>update = {html.escape(json.dumps(update.to_dict(), indent=2, ensure_ascii=False))}'
+        '</pre>\n\n'
+        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
+        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+        f'<pre>{html.escape(tb_string)}</pre>'
+    )
+
+    update.message.reply_html(text=message)
+
 
 def main():
     updater = Updater(TOKEN, use_context=True)
@@ -98,10 +172,13 @@ def main():
 
     dispatcher.add_handler(CommandHandler(
         ['afk', 'back', 'online', 'returned',
-         'todo', 'task', 'show_tasks', 'tasks', 'del_task', 'done'],
+         'todo', 'task', 'show_tasks', 'tasks', 'del_task', 'done',
+         'code'],
         command_handler)
     )
     dispatcher.add_handler(MessageHandler(Filters.text, echoer))
+
+    dispatcher.add_error_handler(error_handler)
 
     # Start the Bot
     updater.start_polling()
